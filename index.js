00282d8dbdbf06ef7b952b4ff6377637ca5760c6
syagawa <yagawa@qzc.co.jp>
2018-08-11 21:36:16 +0900

    Add array length cash

--- index.js
+++ index.js
@@ -5,38 +5,64 @@
   var max = 1000;
   var i;
 
-  console.time("t1");
+  console.time("for before");
   for(i = 0; i < max; i++){
     document.getElementById("div1").innerHTML += i + ", ";
   }
-  console.timeEnd("t1");
+  console.timeEnd("for before");
 
-
-  console.time("t2");
+  console.time("for after");
   var str = "";
   for(i = 0; i < max; i++){
     str += i + ", ";
   }
 
   document.getElementById("div2").innerHTML = str;
-  console.timeEnd("t2");
+  console.timeEnd("for after");
+
 
   // style
-  console.time("t1");
+  console.time("style before");
   for(i = 0; i < max; i++){
     var padding = document.getElementById("div1").style.padding;
     var margin = document.getElementById("div1").style.margin;
   }
-  console.timeEnd("t1");
+  console.timeEnd("style before");
 
-
-  console.time("t2");
+  console.time("style after");
   for(i = 0; i < max; i++){
     var style = document.getElementById("div1").style;
     var padding = style.padding;
     var margin = style.margin;
   }
-  console.timeEnd("t2");
+  console.timeEnd("style after");
+
+
+  // array length cash
+
+  var arr = [];
+  var i;
+  for(i = 0; i < max; i++){
+    arr.push(i);
+  }
+
+
+  console.time("array length cach before");
+  var arr1 = [];
+  for(i = 0; i < arr.length; i++){
+    arr1.push(Math.random());
+  }
+  console.timeEnd("array length cach before");
+
+
+  console.time("array length cach after");
+  var arr2 = [];
+  var len = arr.length;
+  for(i = 0; i < len; i++){
+    arr2.push(Math.random());
+  }
+
+  console.timeEnd("array length cach after");
 
 
 })(window);
