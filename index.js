4f9cebcbc7c7700abe63b103cb30369217587133
syagawa <yagawa@qzc.co.jp>
2018-08-11 21:44:56 +0900

    Add function call

--- index.js
+++ index.js
@@ -64,5 +64,34 @@
 
   console.timeEnd("array length cach after");
 
+  // function call
+
+  var func1 = function () {
+    var func = function (n){
+      return n * n;
+    }
+    var i = 100000;
+    var sum = 0;
+    while(i--){
+      sum += func(i);
+    }
+  };
+
+  var func2 = function () {
+    var i = 100000;
+    var sum = 0;
+    while(i--){
+      sum += i * i;
+    }
+  };
+
+  console.time("function call before");
+  func1();
+  console.timeEnd("function call before");
+
+  console.time("function call after");
+  func2();
+  console.timeEnd("function call after");
+
 
 })(window);
