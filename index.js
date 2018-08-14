ecb93e79a0e449d19990a6148474ee8a21228c73
syagawa <yagawa@qzc.co.jp>
2018-08-14 23:42:52 +0900

    Add clonenode

--- index.js
+++ index.js
@@ -79,6 +79,33 @@
   console.timeEnd("dom createDocumentFragment after");
 
 
+  // dom clonenode
+
+  var max = 10000;
+  var i;
+
+  document.getElementById("div1").innerHTML = "";
+  document.getElementById("div2").innerHTML = "";
+
+  console.time("dom clonenode before");
+  for(i = 0; i < max; i++){
+    var p = document.createElement("p");
+    p.textContent = i + ",";
+    document.getElementById("div1").appendChild(p);
+  }
+  console.timeEnd("dom clonenode before");
+
+  console.time("dom clonenode after");
+  var clone = document.getElementById("div2").cloneNode(true);
+  for(i = 0; i < max; i++){
+    var p = document.createElement("p");
+    p.textContent = i + ",";
+    clone.appendChild(p);
+  }
+  document.getElementById("div2").parentNode.appendChild(clone);
+  console.timeEnd("dom clonenode after");
+
+
   // array length cash
 
   var arr = [];
