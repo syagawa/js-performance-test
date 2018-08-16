9d84ca5c1746a7f11af87257c571c55d69a4c23f
syagawa <yagawa@qzc.co.jp>
2018-08-16 23:31:53 +0900

    Move domDirectOrVariable to lib directory

--- index.js
+++ index.js
@@ -1,28 +1,12 @@
+
 (function(global){
 
-  // // dom direct or variable
-  var domDirectOrVariable = function(){
 
-    var str = "";
-    var max = 1000;
-    var i;
-
-    console.time("dom direct or variable before");
-    for(i = 0; i < max; i++){
-      document.getElementById("div1").innerHTML += i + ", ";
-    }
-    console.timeEnd("dom direct or variable before");
-
-    console.time("dom direct or variable after");
-    var str = "";
-    for(i = 0; i < max; i++){
-      str += i + ", ";
-    }
-
-    document.getElementById("div2").innerHTML = str;
-    console.timeEnd("dom direct or variable after");
-  };
-  domDirectOrVariable();
+  document.getElementById("load_btn").addEventListener("click", function(e){
+    var script = document.createElement("script");
+    script.src = "./lib/domDirectOrVariable.js";
+    document.getElementById("js_src").appendChild(script);
+  });
 
   // style
   console.time("style before");
