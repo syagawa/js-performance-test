452bb5a1a637cbd639c2d743fea6ae6b68649dc9
syagawa <yagawa@qzc.co.jp>
2018-08-22 23:30:54 +0900

    Add APP.timer

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 (function(global){
 
+  global.APP = global.APP ? global.APP : {};
+
+  console.log(APP);
+  console.log("aaa");
+
+  APP.timer = {
+    start_time: "",
+    start: function(){
+      this.start_time = new Date().getTime();
+    },
+    end: function(){
+      var end_time = new Date().getTime();
+      var time = end_time - this.start_time;
+      return time;
+    }
+  };
+
+  console.log(APP);
+
+
   document.getElementById("load_btn").addEventListener("click", function(e){
     var script = document.createElement("script");
 
@@ -8,7 +28,8 @@
     document.getElementById("js_src").appendChild(script);
 
     script.remove();
-
   });
+
+
 
 })(window);
