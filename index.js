663e78fb3ba25f2c6bd0e154aafcb4d681463cc2
syagawa <yagawa@qzc.co.jp>
2018-08-23 23:30:57 +0900

    Add timer.show & timer.hide

--- index.js
+++ index.js
@@ -2,25 +2,44 @@
 
   global.APP = global.APP ? global.APP : {};
 
-  console.log(APP);
-  console.log("aaa");
-
   APP.timer = {
     start_time: "",
     start: function(){
       this.start_time = new Date().getTime();
     },
     end: function(){
+      if(!this.start_time){
+        console.log("Start time is not set.");
+        return false;
+      }
       var end_time = new Date().getTime();
       var time = end_time - this.start_time;
+      this.start_time = "";
       return time;
+    },
+    show: function(arr){
+      var display = document.getElementById("display");
+      var i;
+      var len = arr.length;
+      for(i = 0; i < len; i++){
+        var p = document.createElement("p");
+        p.innerHTML = arr[i];
+        display.appendChild(p);
+      }
+    },
+    hide: function(){
+      var display = document.getElementById("display");
+      display.innerHTML = "";
     }
+
   };
 
-  console.log(APP);
 
 
   document.getElementById("load_btn").addEventListener("click", function(e){
+
+    APP.timer.hide();
+
     var script = document.createElement("script");
 
     var src = document.getElementById("select_src").value;
